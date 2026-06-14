a4d15f2dcf76f2e5d107aa9a37503d663088d1b4
Van Tu <tutai787878@gmail.com>
2026-06-14 11:47:17 +0200

    fixeed order number diplcated

--- booking-order.php
+++ booking-order.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: TCG Restaurant Shop
  * Description: Restaurant Shop for delivery and take away
- * Version: 1.7.2
+ * Version: 1.7.3
  * License: GPLv2 or later
  */
 define('BOOKING_ORDER_PATH', plugin_dir_url(__FILE__));
